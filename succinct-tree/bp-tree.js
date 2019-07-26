8d6c28d0ffa356f90630fa2fa06ff726bbbec08f
kcantrel <kcantrel@ucsd.edu>
2019-07-26 10:22:44 -0700

    added a rank/select

--- succinct-tree/bp-tree.js
+++ succinct-tree/bp-tree.js
@@ -6,11 +6,13 @@ define(['ByteArray'], function(ByteArray) {
     * Initialzes a new BP tree.
     *
     * @param {Uint8Array} bp The array that represents the tree structure
+    * @param {Array} names The names of each node stored in preorder
+    * @param {Array} lengths The lengths of each node stored in preorder
     *
     * @return {BPTree}
     * @constructs BPTree
     */
-    function BPTree(bp) {
+    function BPTree(bp, names=null, lengths=null) {
         /**
          * @type {Uint8Array}
          * Used to store the structure of the tree
@@ -21,13 +23,16 @@ define(['ByteArray'], function(ByteArray) {
          * @type {Number}
          * Number of nodes in tree
          */
-        this.length = this,bp.length / 2;
+        this.size = this,bp.length / 2;
 
-        // Initialize the select r cache arrays
-        var unique = function(array) {
-            var l = array.length;
-            var uniArr = new Uint32Array(l);
-        }
+        /*
+         * @type {Array}
+         * @private
+         * stores the name of each node in preorder
+         */
+        this.names_ = (names) ? names : Array.of(this.size).map(function(v,i) {
+                                            return "" + i;
+                                        });
 
         /**
          * @type {Uint32Array}
@@ -63,8 +68,77 @@ define(['ByteArray'], function(ByteArray) {
          */
         this.s1Cache_ = ByteArray.seqUniqueIndx(this.r1Cache_, Uint32Array);
 
+        /**
+         * @type {Uint32Array}
+         * @private
+         * excess is used frequently so we can cache it to get a slight
+         * performance boost
+         */
+        var eCache = [];
+        for (var i = 0; i < this.bp.length; i++) {
+            eCache.push((2 * this.r1Cache_[i]) - i - 1);
+        }
+        this.eCache_ = new Uint32Array(eCache);
+    };
 
+    /**
+     *
+     * Returns the number of times bit t was observed leading upto bit t
+     *
+     * @param{Number} t Bit value, 0 is an close paraenthesis and 1 is an
+     *      open parenthesis.
+     * @param{Number} i Position to evaluate
+     *
+     * @return{Number}
+     */
+    BPTree.prototype.rank = function(t, i) {
+        var rCache = (t) ? this.r1Cache_ : this.r0Cache_;
+        return rCache[i];
+    };
+
+    /**
+    *
+    * Returns the position of the kth occurance of bit t
+    *
+    * @param{Number} t Bit value, 0 is an close paraenthesis and 1 is an
+     *      open parenthesis.
+     * @param{Number} k Note k starts at 0
+     *
+     * @return{Number}
+     */
+    BPTree.prototype.select = function(t, k) {
+        var sCache = (t) ? this.s1Cache_ : this.s0Cache_;
+        return sCache[k];
+    };
+
+    /**
+     *
+     * The excess at position i is the number of 1's - 0's.
+     * i.e. rank(1,i) - rank(0,i)
+     *
+     * @oaram{Number} i The position to calculate the excess
+     *
+     * @return{Number} The excess at position i
+     * @private
+     */
+    BPTree.prototype.excess_ = function(i) {
+        // need to subtract 1 since i starts at 0
+        // Note: rank(1,i) - rank(0,i) = (2*(rank(1,i)) - i
+        return (2 * this.r1Cache_[i]) - i - 1;
+    };
+
+    /**
+     *
+     * The depth of a node i in preorder
+     *
+     * @param{Number} i Node i in preorder
+     *
+     * @return{Number} Depth of node i
+     */
+    BPTree.prototype.depth = function(i) {
+        //depth is same as excess
+        return this.eCache_[i];
     };
 
     return BPTree;
-  });
+});
--- tests/test-bp-tree.js
+++ tests/test-bp-tree.js
@@ -3,11 +3,16 @@ require(['jquery', 'ByteArray', 'BPTree'], function($, ByteArray, BPTree) {
 
         // test variables
         var bpArray;
+        var bpObj;
 
+        // Setup test variables
+        // Note: This is ran for each test() so tests can modify bpArray without
+        // effecting other test
         module('Succinct Tree' , {
             setup: function() {
-                bpArray = new Uint8Array([1, 1, 1, 0, 1, 0, 1, 1 ,0, 0, 0, 1, 0, 1, 1,
-                        1, 0, 1, 0, 0, 0, 0]);
+                bpArray = new Uint8Array([1, 1, 1, 0, 1, 0, 1, 1 ,0, 0, 0, 1, 0,
+                        1, 1, 1, 0, 1, 0, 0, 0, 0]);
+                bpObj = new BPTree(bpArray);
             },
 
             teardown: function() {
@@ -17,8 +22,6 @@ require(['jquery', 'ByteArray', 'BPTree'], function($, ByteArray, BPTree) {
 
         // tests the constructor of bp tree
         test('Test BP Constructor', function() {
-            var bpObj = new BPTree(bpArray);
-
             // Test if rank 0 cache was initialized correctly
             var r0Cache = ByteArray.sumVal(bpArray, Uint32Array, 0);
             deepEqual(bpObj.r0Cache_, r0Cache, 'Test: rank 0 cache');
@@ -33,6 +36,53 @@ require(['jquery', 'ByteArray', 'BPTree'], function($, ByteArray, BPTree) {
 
             // Test if select 1 cached was initialized correctly
             var s1Cache = ByteArray.seqUniqueIndx(r1Cache, Uint32Array);
+            deepEqual(bpObj.s1Cache_, s1Cache, 'Test: select 1 cache');
+        });
+
+        test('Test rank property', function() {
+            var BIT_0 = 0, BIT_1 = 1;
+            var l = bpArray.length;
+            for (var i = 0; i < l; i++) {
+                equal(bpObj.rank(BIT_0, i) + bpObj.rank(BIT_1, i), i + 1);
+            }
+        });
+
+        test('Test select property', function() {
+            var s0 = ByteArray.seqUniqueIndx(
+                        ByteArray.sumVal(bpArray, Uint32Array, 0),
+                        Uint32Array);
+            var s1 = ByteArray.seqUniqueIndx(
+                        ByteArray.sumVal(bpArray, Uint32Array, 1),
+                        Uint32Array);
+            var s = {0 : s0, 1: s1};
+            for (var bit in s) {
+                var ks = s[bit];
+                var t = parseInt(bit);
+                for(var k = 0; k < ks.length; k++) {
+                    equal(bpObj.rank(t, bpObj.select(t, k)), k + 1);
+                }
+            }
+        });
+
+        test('Test excess', function() {
+            var exp = [1, 2, 3, 2, 3, 2, 3, 4, 3, 2, 1, 2, 1, 2, 3, 4, 3, 4, 3,
+                       2, 1, 0];
+            console.log({rank0Cache: bpObj.r0Cache_,
+                        rank1Cache: bpObj.r1Cache_});
+            for (var i = 0; i < exp.length; i++) {
+                equal(bpObj.excess_(i), exp[i]);
+            }
+        });
+
+        // Note: depth should equal to excess
+        test('Test depth', function() {
+            var exp = [1, 2, 3, 2, 3, 2, 3, 4, 3, 2, 1, 2, 1, 2, 3, 4, 3, 4, 3,
+                       2, 1, 0];
+            console.log({rank0Cache: bpObj.r0Cache_,
+                        rank1Cache: bpObj.r1Cache_});
+            for (var i = 0; i < exp.length; i++) {
+                equal(bpObj.depth(i), exp[i]);
+            }
         });
     });
 });
